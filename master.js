56d13b063140e7aa7300d5f2f5e9991c8d407393
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-20 10:15:47 -0500

    [+] Jade: Vistas

--- master.js
+++ master.js
@@ -1,29 +1,54 @@
 const express = require( 'express' ),
+      path = require( 'path' ),
       app = express();
 
+/** Rutas estaticas disponibles para el proyecto de forma privada */
+app .set( 'views', __dirname + '/views' );          // Vistas         
+app .set( 'view engine', 'jade' );                  // Generated Templates (Entiende nombre de archivos sin extension .jade)
+
+/** Crea Middleware: Rutas estaticas disponibles para el proyecto de forma publica usando Express  */
+app .use( express .static( path .join( __dirname, 'public' ) ) );      
+
+/** Rutas disponibles */
 app .get( '/', ( require, response ) => {
-    response .send( 'Home' );
+    response .render( 'home', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Home'
+    });
 });
 app .get( '/noticias', ( require, response ) => {
-    response .send( 'Noticias' );
+    response .render( 'news', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Noticias'
+    });
 });
 app .get( '/opinion', ( require, response ) => {
-    response .send( 'Opinion' );
+    response .render( 'opinion', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Opinion'
+    });
 });
 app .get( '/economia', ( require, response ) => {
-    response .send( 'Economia' );
+    response .render( 'economy', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Economia'
+    });
 });
 app .get( '/deportes', ( require, response ) => {
-    response .send( 'Deportes' );
+    response .render( 'sports', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Deportes'
+    });
 });
 app .get( '/entretenimiento', ( require, response ) => {
-    response .send( 'Entretenimiento' );
+    response .render( 'enternaiment', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Entretenimiento'
+    });
 });
 app .get( '/cultura', ( require, response ) => {
-    response .send( 'Cultura' );
+    response .render( 'culture', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Cultura'
+    });
 });
 app .get( '/tecnologia', ( require, response ) => {
-    response .send( 'Tecnologia' );
+    response .render( 'tecnology', {                     // nombre del archivo .jade (Ver linea 8)
+        title: 'Tecnologia'
+    });
 });
 
 app .listen( 8081 );
