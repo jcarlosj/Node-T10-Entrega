4f62fad62328eea782d2dcb399838adef430a1ab
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-08-20 19:04:36 -0500

    [+] Despliega en la vista datos a partir de archivo JSON

--- master.js
+++ master.js
@@ -1,4 +1,5 @@
 const express = require( 'express' ),
+      bodyParser = require( 'body-parser' ),
       fs = require( 'fs' ),
       path = require( 'path' ),
       app = express();
@@ -8,7 +9,11 @@ app .set( 'views', __dirname + '/views' );          // Vistas
 app .set( 'view engine', 'jade' );                  // Generated Templates (Entiende nombre de archivos sin extension .jade)
 
 /** Crea Middleware: Rutas estaticas disponibles para el proyecto de forma publica usando Express  */
-app .use( express .static( path .join( __dirname, 'public' ) ) );      
+app .use( express .static( path .join( __dirname, 'public' ) ) );   
+
+/** Configura Express con 'body-parser' incluido datos JSON */ 
+app .use( bodyParser .json() );
+app .use( bodyParser .urlencoded({ extended: true }) );
 
 /** Importa Rutas */
 const routes = require( './routes/routes.js' )( app, fs );
--- routes/routes.js
+++ routes/routes.js
@@ -1,42 +1,54 @@
 const appRouter = ( app, fs ) => {
+    const dataPath = './data/users.json';
+
+    console .log( 'JSON > Users', JSON .stringify( dataPath ) );
 
     /** Rutas disponibles */
-    app .get( '/', ( require, response ) => {
-        response .render( 'home', {                     // nombre del archivo .jade (Ver linea 8)
-            title: 'Home'
+    app .get( '/', ( request, response ) => {
+        fs .readFile( dataPath, 'utf8', ( err, data ) => {
+            if ( err ) {
+                throw err;
+            }
+
+            //response .send( JSON .parse( data ) );
+            response .render( 'home', {                     // nombre del archivo .jade (Ver linea 8)
+                data: JSON .parse( data ),
+                title: 'Home'
+            });
         });
+
     });
-    app .get( '/noticias', ( require, response ) => {
+    app .get( '/noticias', ( request, response ) => {
         response .render( 'news', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Noticias'
         });
     });
-    app .get( '/opinion', ( require, response ) => {
+    app .get( '/opinion', ( request, response ) => {
         response .render( 'opinion', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Opinion'
         });
     });
-    app .get( '/economia', ( require, response ) => {
+    app .get( '/economia', ( request, response ) => {
         response .render( 'economy', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Economia'
         });
     });
-    app .get( '/deportes', ( require, response ) => {
+    app .get( '/deportes', ( request, response ) => {
         response .render( 'sports', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Deportes'
         });
     });
-    app .get( '/entretenimiento', ( require, response ) => {
+    app .get( '/entretenimiento', ( request, response ) => {
         response .render( 'enternaiment', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Entretenimiento'
         });
     });
-    app .get( '/cultura', ( require, response ) => {
+    app .get( '/cultura', ( request, response ) => {
         response .render( 'culture', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Cultura'
         });
     });
-    app .get( '/tecnologia', ( require, response ) => {
+    app .get( '/tecnologia', ( request, response ) => {
         response .render( 'tecnology', {                     // nombre del archivo .jade (Ver linea 8)
             title: 'Tecnologia'
         });
